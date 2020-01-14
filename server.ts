b8852adcd4e182c1b1f88a7702535c951bb2357a
Kai Shoya <s-kai@lumber-mill.co.jp>
2020-01-14 18:02:02 +0900

    tsconfigを書き換えた事で動かなくなっていたためserver.tsを修正 ts-nodeをシンプルな記述に戻した

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ const app = require('express')()
 const path = require('path')
 const PORT = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
+app.get('/', (req: any, res: any) => {
   res.sendFile(path.join(__dirname + '/index.html'))
 })
 
